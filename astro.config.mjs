bd307f403aea4bb2d7466852c23ca41d17fad96c
Vic <cvic@example.com>
2025-08-09 18:23:51 +0300

    Add initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,37 @@
-// @ts-check
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare()
-}); ``
+  // Set your site's URL
+  site: 'https://astro-now.cvic.workers.dev',
+
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+
+  integrations: [
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: DEFAULT_LOCALE_SETTING,
+        locales: Object.fromEntries(
+          Object.entries(LOCALES_SETTING).map(
+            ([key, value]) => [key, value.lang ?? key]
+          )
+        ),
+      },
+    })
+  ],
+
+  adapter: cloudflare(),
+});
